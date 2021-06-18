667facc7a3d0e8a9ea5cad4d6beed09c6feb74bf
David Rochus <david.rochus@gmail.com>
2021-06-18 16:25:41 +0200

    Updates

---
+++ precache-manifest.526defc0fdc438e37549ef133ad99e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfade207d1850c910cb6d2df465e71e",
+    "url": "/alyra-defi4/index.html"
+  },
+  {
+    "revision": "fd631bb62cf63a0b8b8a",
+    "url": "/alyra-defi4/static/css/2.54c80412.chunk.css"
+  },
+  {
+    "revision": "c968da95e33942729733",
+    "url": "/alyra-defi4/static/css/main.a3487775.chunk.css"
+  },
+  {
+    "revision": "fd631bb62cf63a0b8b8a",
+    "url": "/alyra-defi4/static/js/2.08b0488d.chunk.js"
+  },
+  {
+    "revision": "c968da95e33942729733",
+    "url": "/alyra-defi4/static/js/main.9abd3055.chunk.js"
+  },
+  {
+    "revision": "6365691731022f5dbad0",
+    "url": "/alyra-defi4/static/js/runtime-main.30bf0fbf.js"
+  }
+]);
